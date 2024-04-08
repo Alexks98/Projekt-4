108ae95b2f997ab258f0620186de026b1824cdd5
kacurjjj <153917404+kacurjjj@users.noreply.github.com>
2024-04-08 20:29:18 +0200

    TRANSLATOR

---
+++ translator.js
@@ -0,0 +1,40 @@
+//This is for the English - Danish Translation of Texts
+const languageData={
+    english:{
+        NavTitle1: 'WHAT WE DO',
+        NavTitle2: 'WHO WE ARE',
+        NavTitle3: 'WHERE WE WORK',
+        NavTitle4: 'GET INVOLVED',
+
+        JoinUs: 'JOIN US',
+
+        Globe: '9 COUNTRIES',
+        HandShake: '40 PARTNERS',
+        Members: '135 MEMBERS',
+        Awards: '19 AWARDS',
+
+        AboutDescription: 'GAME strives for a world with equal opportunities for all children and youth. GAME is an international NGO working for social change through youth-led street sports and culture in Africa, Europe and the Middle East.',
+        AboutReadMore: 'READ MORE',
+
+        FightForTitle: 'GAME FIGHTS FOR',
+        Empowerment: 'Empowerment',
+        Leadership: 'Leadership',
+        Sustainability: 'Sustainability',
+        Innovation: 'Innovation',
+
+        SupportGame: 'WANT TO SUPPORT GAME',
+        Donation: 'Make a donation',
+        BecomePartner: 'Become a business partner',
+        SupportWebShop: 'Support our webshop where',
+        ProgrammeDonor: 'Become a program donor something',
+
+        StoriesGane: 'STORIES FROM GAME',
+
+        Contact: 'CONTACT',
+        Booking: 'BOOKING',
+        Press: 'PRESS',
+        DonorsPartners: 'LIST OF DONORS AND PARTNERS',
+        JobInternship: 'JOBS AND INTERNSHIPS',
+        WebShop: 'WEBSHOPS',
+    }
+}
