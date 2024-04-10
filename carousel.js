e226ff20fb0f251b8d06b1b279878d294f76a933
ramantra <rmroomkh70@gmail.com>
2024-04-10 12:28:54 +0200

    carousel functionality

--- carousel.js
+++ carousel.js
@@ -4,30 +4,23 @@ const galleryControls = ['previous', 'next'];
 const galleryItems = document.querySelectorAll('.gallery-item');
 
 class Carousel {
-
-    constructor(container, items, controls){
+    constructor(container, items, controls) {
         this.carouselContainer = container;
         this.carouselControls = controls;
         this.carouselArray = [...items];
     }
 
-    updateGallery(){
-        this.carouselArray.forEach(el => {
-            el.classList.remove('gallery-item-1');
-            el.classList.remove('gallery-item-2');
-            el.classList.remove('gallery-item-3');
-            el.classList.remove('gallery-item-4');
-            el.classList.remove('gallery-item-5');
-        });
-
-        this.carouselArray.slice(0, 5).forEach((el, i) => {
+    updateGallery() {
+        this.carouselArray.forEach((el, i) => {
+            el.classList.remove('gallery-item-1', 'gallery-item-2', 'gallery-item-3', 'gallery-item-4', 'gallery-item-5');
             el.classList.add(`gallery-item-${i+1}`);
         });
     }
-    setCurrentState(direction){
-        if (direction.className == 'gallery-controls-previous'){
+
+    setCurrentState(direction) {
+        if (direction.classList.contains('gallery-controls-previous')) {
             this.carouselArray.unshift(this.carouselArray.pop());
-        } else{
+        } else {
             this.carouselArray.push(this.carouselArray.shift());
         }
         this.updateGallery();
@@ -35,23 +28,25 @@ class Carousel {
     
     setControls() {
         this.carouselControls.forEach(control => {
-            galleryControlsContainer.appendChild(document.createElement('button')).className = 'gallery-controls-${control}';
-            document.querySelector('.gallery-controls-${control}').innertext = control;
+            const button = document.createElement('button');
+            button.className = `gallery-controls-button gallery-controls-${control}`;
+            button.innerText = control;
+            button.setAttribute('aria-label', control);
+            galleryControlsContainer.appendChild(button);
         });
     }
     
-    useControls(){
-        const triggers = [...galleryControlsContainer.childNodes];
-        triggers.forEach(control => {
-            control.addEventListner('click', e => {
+    useControls() {
+        this.carouselControls.forEach(control => {
+            const trigger = document.querySelector(`.gallery-controls-${control}`);
+            trigger.addEventListener('click', (e) => {
                 e.preventDefault();
-                this.setCurrentState(control);
+                this.setCurrentState(trigger);
             });
         });
     }
 }
 
 const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
-
 exampleCarousel.setControls();
 exampleCarousel.useControls();
