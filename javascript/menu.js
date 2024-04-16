3965da9acfffc13e0db0f61a96c4574dafebd8d2
Alexks98 <alexandra.sorensen@hotmail.com>
2024-04-16 08:38:30 +0200

    fjernet  kommentarer

--- javascript/menu.js
+++ javascript/menu.js
@@ -12,12 +12,12 @@ const hamburgerEvent = (navigation, close, open) => {
 //if elese//
   if (navigation === 'flex') {
       navigationItems.classList.add('active');
-      // Skjul slogan og "Join Us" -knappen når hamburgermenuen er åben
+      
       document.querySelector('.slogan').style.display = 'none';
       document.querySelector('.CTA').style.display = 'none';
   } else {
       navigationItems.classList.remove('active');
-      // Vis slogan og "Join Us" -knappen når hamburgermenuen er lukket
+      
       document.querySelector('.slogan').style.display = 'block';
       document.querySelector('.CTA').style.display = 'block';
   }
@@ -25,5 +25,5 @@ const hamburgerEvent = (navigation, close, open) => {
 };
 openHam.addEventListener('click', () => hamburgerEvent("flex", "block", "none"));
 closeHam.addEventListener('click', () => hamburgerEvent("none", "none", "block"));
-//  mangler arismatiske Operatorer, Arrays, Loops, //
+//  mangler Arrays, Loops, //
 
