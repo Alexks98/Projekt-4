409753865037568392c0511273c9c99c996ee6b4
ramantra <rmroomkh70@gmail.com>
2024-04-17 13:10:20 +0200

    edits

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -1,17 +1,17 @@
 const galleryContainer = document.querySelector('.gallery-container'); //variables/
-const galleryControlsContainer = document.querySelector('.gallery-controls'); //variables/
+const galleryControlsContainer = document.querySelector('.gallery-controls'); //variables//
 const galleryControls = ['previous', 'next']; //an array//
-const galleryItems = document.querySelectorAll('.gallery-item'); //variables/
+const galleryItems = document.querySelectorAll('.gallery-item'); //variables// //retrieves the imaages within the gallery container through DOM//
 
 class Carousel {
 
     constructor(container, items, controls) {
-        this.carouselContainer = container;
-        this.carouselControls = controls;
-        this.carouselArray = [...items];
+        this.carouselContainer = container; //reference to the DOM element that acts as the container for the carousel images//
+        this.carouselControls = controls; // reference the control elements, such as the buttons for navigating the carousel//
+        this.carouselArray = [...items];  //Here, the spread operator ('…') is used to create a new array from items, which represents the individual images in the carousel//
     }
 
-    //Loop number 1//
+    //Loop number 1 + function//
     updateGallery() {
         this.carouselArray.forEach(el => {
             el.classList.remove('gallery-item-1', 'gallery-item-2', 'gallery-item-3', 'gallery-item-4', 'gallery-item-5');
@@ -24,24 +24,26 @@ class Carousel {
 
     //if-else control structure//
     setCurrentState(control) {
+        //Checks//
         if (control.classList.contains('gallery-controls-previous')) {
-            this.carouselArray.unshift(this.carouselArray.pop());
+            this.carouselArray.unshift(this.carouselArray.pop()); //Action When Condition is True//
         } else {
-            this.carouselArray.push(this.carouselArray.shift());
+            this.carouselArray.push(this.carouselArray.shift()); //Action when condition is false using else//
         }
         this.updateGallery();
     }
     
+    //function//
     setControls() {
         this.carouselControls.forEach(control => {
             const button = document.createElement('button');
             button.className = `gallery-controls-${control}`;
-            button.setAttribute('aria-label', control); // Accessibility for screen readers
+            button.setAttribute('aria-label', control); // Accessibility for screen readers//
             galleryControlsContainer.appendChild(button);
         });
     }
     
-    //Loop number 2//
+    //Loop number 2 + Event handling//
     useControls() {
         const triggers = [...galleryControlsContainer.childNodes];
         triggers.forEach(control => {
