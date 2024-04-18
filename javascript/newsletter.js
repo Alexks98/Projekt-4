15eca6fb8996ab0109f85cbbc25163bee98a41a8
CecilieBirkholm <birkholm010502@gmail.com>
2024-04-18 10:33:14 +0200

    gem mail

--- javascript/newsletter.js
+++ javascript/newsletter.js
@@ -1,15 +1,29 @@
-
 const nyhedsbrevForm = document.getElementById('nyhedsbrevForm').addEventListener('submit', function(e) {
- // Forhindre formen i at submitte traditionelt
+    e.preventDefault(); // Forhindrer formen i at blive indsendt traditionelt
 
- alert('Tak fordi du tilmelde dig vores nyhedsbrev!');
- // Laver en popup besked om tak for tilmelding
-
- // Bruges til at finde fejl
+    // Hent e-mail-adressen fra inputfeltet
     var email = document.getElementById('email').value;
-    console.log('Email:', email);
 
-   // Clear inputfeltet efter tilmelding
-   document.getElementById('email').value = '';
+    // Gem e-mail-adressen i localStorage
+    gemEmail(email);
+
+    // Vis en bekræftelsesbesked til brugeren
+    alert('Tak for din tilmelding til vores nyhedsbrev!');
+
+    // Brug e-mail-adressen i din kode, hvis nødvendigt
+    console.log('Email:', email);
 });
 
+function gemEmail(email) {
+    // Gem e-mail-adressen i localStorage
+    localStorage.setItem('nyhedsbrevEmail', email);
+    console.log('E-mail gemt:', email);
+}
+
+// Hent data fra localStorage
+let gemtEmail = localStorage.getItem('nyhedsbrevEmail');
+if (gemtEmail !== null) {
+    console.log('Gemt e-mail fra localStorage:', gemtEmail);
+} else {
+    console.log('Ingen e-mail gemt i localStorage.');
+}
